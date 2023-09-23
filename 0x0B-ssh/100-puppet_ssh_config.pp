8cec0234a494fc4133a578715a6bf4b1390379e8
s-maarouf <maarouf.salahedd@gmail.com>
2023-09-23 14:19:40 -0700

    Completed task 4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,9 +2,6 @@
 
 file { '/home/ubuntu/.ssh/config':
   ensure  => file,
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  mode    => '0600',
   content => "Host *\n    IdentityFile ~/.ssh/school\n    PasswordAuthentication no\n",
 }
 
